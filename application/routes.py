65eb3ba0a113b5ca9ff8d4d62f66db43fef1c2bd
Hamza Yacub <hamzayacub97@gmail.com>
2020-05-14 09:38:43 +0000

    update models

--- application/routes.py
+++ application/routes.py
@@ -17,10 +17,9 @@ def post():
     form = PostForm()
     if form.validate_on_submit():
         postData = Posts(
-            first_name = form.first_name.data,
-            last_name = form.last_name.data,
             title = form.title.data,
-            content = form.content.data
+            content = form.content.data,
+            author = current_user
         )
 
         db.session.add(postData)
@@ -41,11 +40,15 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hash_pw = bcrypt.generate_password_hash(form.password.data)
 
         user = Users(
+                first_name = form.first_name.data,
+                last_name = form.last_name.data,
                 email = form.email.data,
                 password = hash_pw
         )
